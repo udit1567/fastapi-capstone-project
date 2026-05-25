186152d7a79ed193ef53af68e2d2d25e07cc6ef7
udit1567 <uditmaurya2006@gmail.com>
2026-05-25 12:11:49 +0530

    added redis caching

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from prometheus_fastapi_instrumentator import Instrumentator
 from app.api import routes_auth, routes_predict
 from app.middleware.logging_middleware import LoggingMiddleware
-from app.core.exceptions import register_exception_handlers
+from app.core.exception import register_exception_handlers
 
 app = FastAPI(title='Car Price Prediction API')
 
